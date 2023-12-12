96d6402fded9cdeb31f19927fe71b3d082fcb804
Владислав <akter.res.hak@gmail.com>
2023-12-12 22:08:45 +0700

    проверьте ДЗ

---
+++ src/main/java/org/example/service/IntegerServiceImpl.java
@@ -0,0 +1,194 @@
+package org.example.service;
+
+import org.example.exceptions.FullException;
+import org.example.exceptions.InvalidIndexException;
+import org.example.exceptions.NullItemException;
+
+import java.util.Arrays;
+
+public class IntegerServiceImpl implements IntegerListService {
+    private final Integer[] storage;
+    private Integer size;
+
+    public IntegerServiceImpl() {
+        storage = new Integer[10];
+        size = 0;
+    }
+
+    public IntegerServiceImpl(Integer initSize) {
+        storage = new Integer[initSize];
+        size = 0;
+    }
+
+    @Override
+    public Integer add(Integer item) {
+        validateSize();
+        validateItem(item);
+        storage[size++] = item;
+        return item;
+    }
+
+    @Override
+    public Integer add(int index, Integer item) {
+        validateSize();
+        validateIndex(index);
+        validateItem(item);
+        if (index == size) {
+            storage[size++] = item;
+            return item;
+        }
+        System.arraycopy(
+                storage, index, storage,
+                index + 1, size - index
+        );
+        storage[index] = item;
+        size++;
+        return item;
+    }
+
+    @Override
+    public Integer set(int index, Integer item) {
+        validateIndex(index);
+        validateItem(item);
+        storage[index] = item;
+        return item;
+    }
+
+    @Override
+    public Integer remove(Integer item) {
+        validateItem(item);
+        int index = indexOf(item);
+
+        return remove(index);
+    }
+
+    @Override
+    public Integer remove(int index) {
+        validateIndex(index);
+        Integer item = storage[index];
+        if (index != size) {
+            System.arraycopy(
+                    storage, index + 1,
+                    storage, index, size - (index + 1)
+            );
+        }
+        size--;
+        return item;
+    }
+
+    @Override
+    public boolean contains(Integer item) {
+        Integer[] storageCopy = toArray();
+        sort(storageCopy);
+        return binarySearch(storageCopy, item);
+    }
+
+    @Override
+    public int indexOf(Integer item) {
+        for (int i = 0; i < size; i++) {
+            if (storage[i].equals(item)) {
+                return i;
+            }
+        }
+        return -1;
+    }
+
+    @Override
+    public int lastIndexOf(Integer item) {
+        for (int i = size - 1; i >= 0; i--) {
+            if (storage[i].equals(item)) {
+                return i;
+            }
+        }
+        return -1;
+    }
+
+    @Override
+    public Integer get(int index) {
+        validateIndex(index);
+        return storage[index];
+    }
+
+    @Override
+    public boolean equals(IntegerListService otherList) {
+        return Arrays.equals(this.toArray(), otherList.toArray());
+    }
+
+    @Override
+    public int size() {
+        return size;
+    }
+
+    @Override
+    public boolean isEmpty() {
+        return size == 0;
+    }
+
+    @Override
+    public void clear() {
+        size = 0;
+    }
+
+    @Override
+    public Integer[] toArray() {
+        return Arrays.copyOf(storage, size);
+    }
+
+    private void validateItem(Integer item) {
+        if (item == null) {
+            throw new NullItemException();
+        }
+    }
+
+    private void validateSize() {
+        if (size == storage.length) {
+            throw new FullException();
+        }
+    }
+
+    private void validateIndex(int index) {
+        if (index < 0 || index >= size) {
+            throw new InvalidIndexException();
+        }
+    }
+
+    public static void sort(Integer[] arr) {
+        for (int i = 1; i < arr.length; i++) {
+            int temp = arr[i];
+            int j = i;
+            while (j > 0 && arr[j - 1] >= temp) {
+                arr[j] = arr[j - 1];
+                j--;
+            }
+            arr[j] = temp;
+        }
+    }
+
+    public static boolean binarySearch(Integer[] arr, int item) {
+        int min = 0;
+        int max = arr.length - 1;
+
+        while (min <= max) {
+            int mid = (min + max) / 2;
+
+            if (item == arr[mid]) {
+                return true;
+            }
+
+            if (item < arr[mid]) {
+                max = mid - 1;
+            } else {
+                min = mid + 1;
+            }
+        }
+        return false;
+    }
+
+    @Override
+    public String toString() {
+        return "IntegerListService{" +
+                "storage=" + Arrays.toString(storage) +
+                ", size=" + size +
+                '}';
+    }
+}
